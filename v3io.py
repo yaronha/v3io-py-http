f29604fabf75d503dde5531b909f83ea0fe6b6fc
Yaron Haviv <yaronh@iguaz.io>
2018-10-10 12:57:25 +0300

    defaults from env

--- v3io.py
+++ v3io.py
@@ -7,11 +7,11 @@ import json
 import os
 
 class v3io:
-    def __init__(self, address, user, password, container):
+    def __init__(self, address='', user='', password='', container=''):
         address = address or environ.get('V3IO_WEBAPI_SERVICE_HOST')
         user = user or environ.get('V3IO_USERNAME', 'iguazio')
         password = password or environ.get('V3IO_PASSWORD')
-        self.url = "http://" + address + "/"
+        self.url = "http://" + address + ":8081/"
         self.auth = (user, password)
         if container and container != "" :
             self.container = container + '/'
